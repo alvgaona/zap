828a8c43be63dee329802e4234beaca628dc93fe
Alvaro Gaona <alvgaona@gmail.com>
2026-03-08 15:07:04 +0100

    chore: add release infrastructure and switch to Apache-2.0
    - Add version macros to zap.h (0.3.0)
    - Switch license from LGPL-3.0/MIT to Apache-2.0
    - Add git-cliff config for changelog generation
    - Add release workflow with GitHub Release, rattler-build, and prefix.dev
    - Add rattler-build recipe for prefix.dev packaging
    - Update README badges and license
    - Ignore markdown files (except README) and .claude directory

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Zap :zap:
+# Zap
 
-[![CI](https://github.com/alvgaona/zap/actions/workflows/ci.yml/badge.svg)](https://github.com/alvgaona/zap/actions/workflows/ci.yml)
-[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](LICENSE)
+[![ci](https://github.com/alvgaona/zap/actions/workflows/ci.yml/badge.svg)](https://github.com/alvgaona/zap/actions/workflows/ci.yml)
+[![license](https://img.shields.io/github/license/alvgaona/zap)](LICENSE)
+[![release](https://img.shields.io/github/v/release/alvgaona/zap)](https://github.com/alvgaona/zap/releases/latest)
+[![prefix.dev](https://img.shields.io/badge/prefix.dev-zap-blue)](https://prefix.dev/channels/zap)
 
 Single-header benchmarking library for C.
 
 ## License
 
-LGPL-3.0
+Apache-2.0
--- zap.h
+++ zap.h
@@ -30,11 +30,16 @@
  *       zap_group_finish(g);
  *   }
  *
- * LICENSE: MIT (see end of file)
+ * LICENSE: Apache-2.0 (see LICENSE file)
  */
 
 #ifndef ZAP_H
 #define ZAP_H
+
+#define ZAP_VERSION_MAJOR 0
+#define ZAP_VERSION_MINOR 3
+#define ZAP_VERSION_PATCH 0
+#define ZAP_VERSION "0.3.0"
 
 #ifdef __cplusplus
 extern "C" {
@@ -2775,26 +2780,4 @@ void zap_compare_group_finish(zap_compare_group_t* g) {
 
 #endif /* ZAP_H */
 
-/*
- * MIT License
- *
- * Copyright (c) 2024
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+/* Licensed under Apache-2.0. See LICENSE file for details. */
